b86280478109b65d64fb8b632544643c4107c2e6
Jose Santamaria <santamaria0323@gmail.com>
2021-02-24 16:57:40 -0300

    Configuraciones iniciales listas

--- models/server.js
+++ models/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
 
         // Middlewares
         this.middlewares();
@@ -12,14 +14,18 @@ class Server {
     }
 
     middlewares() {
+        // CORS
+        this.app.use(cors());
+
+        // Lectura y parse del body
+        this.app.use(express.json());
+
         // Directorio público
         this.app.use(express.static('public'));
     }
 
     routes() {
-        this.app.get('/api', (req, res) => {
-            res.send('Hola Mundo!');
-        });
+        this.app.use(this.usuariosPath, require('../routes/usuarios'));
     }
 
     listen() {
